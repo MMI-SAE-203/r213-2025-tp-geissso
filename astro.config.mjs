f4b8d28f1e591060e9e4ec7bac749ebe6a94e14c
Geiss Solène <solene.geiss@edu.univ-fcomte.fr>
2025-01-24 14:35:44 +0100

    ezjugterudhetiu

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import tailwind from '@astrojs/tailwind';
 
 import netlify from '@astrojs/netlify';
 
+import alpinejs from '@astrojs/alpinejs';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), alpinejs()],
   adapter: netlify(),
   experimental:{svg:true}
 });
