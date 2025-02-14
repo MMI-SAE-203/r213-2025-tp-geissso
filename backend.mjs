5429f648232c7ee660d4a945371a40db155e72ab
Geiss Solène <solene.geiss@edu.univ-fcomte.fr>
2025-02-14 11:31:18 +0100

    creation formulaire

--- backend.mjs
+++ backend.mjs
@@ -27,3 +27,18 @@ export async function getOffre(id) {
         return null;
     }
 }
+export async function addOffre(house) {
+    try {
+        await pb.collection('Maison').create(house);
+        return {
+            success: true,
+            message: 'Offre ajoutée avec succès'
+        };
+    } catch (error) {
+        console.log('Une erreur est survenue en ajoutant la maison', error);
+        return {
+            success: false,
+            message: 'Une erreur est survenue en ajoutant la maison'
+        };
+    }
+}
